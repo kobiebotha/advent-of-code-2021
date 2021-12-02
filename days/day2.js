5888c05e0138fc2fa2692610756f0f26cbcde4ff
Kobie Botha <kobie@journeyapps.com>
2021-12-02 16:14:46 -0700

    day2

--- days/day2.js
+++ days/day2.js
@@ -1,9 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = void 0;
+const fs = require('fs');
+function readInputs() {
+    let entries = [];
+    try {
+        const data = fs.readFileSync('./inputs/day2', 'utf8');
+        entries = data.split('\n');
+    }
+    catch (err) {
+        console.error(err);
+    }
+    return entries;
+}
 function run() {
     console.log('this is day 2');
-    console.log('test');
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+    let entries = readInputs();
+    entries.forEach(element => {
+        switch (element[0]) {
+            case 'f':
+                let deltaX = parseInt(element[element.length - 1]);
+                horizontal += deltaX;
+                depth += deltaX * aim;
+                break;
+            case 'd':
+                aim += parseInt(element[element.length - 1]);
+                break;
+            case 'u':
+                aim -= parseInt(element[element.length - 1]);
+                break;
+            default:
+                break;
+        }
+    });
+    console.log(`total = ${horizontal * depth}`);
 }
 exports.run = run;
 //# sourceMappingURL=day2.js.map
--- days/day2.ts
+++ days/day2.ts
@@ -1,5 +1,46 @@
+const fs = require('fs')
+
+function readInputs(): string[] {
+    let entries: string[] = [];
+
+    try {
+        const data = fs.readFileSync('./inputs/day2', 'utf8');
+
+        entries = data.split('\n');
+        
+      } catch (err) {
+        console.error(err)
+      }
+
+    return entries;
+}
+
 export function run() {
     console.log('this is day 2');
 
-    console.log('test');
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+
+    let entries = readInputs();
+
+    entries.forEach(element => {
+        switch (element[0]) {
+            case 'f':
+                let deltaX = parseInt(element[element.length-1]);
+                horizontal+= deltaX;
+                depth+=deltaX*aim;
+                break;
+            case 'd':
+                aim+=parseInt(element[element.length-1]);
+                break;
+            case 'u': 
+                aim-=parseInt(element[element.length-1]);
+                break;
+            default:
+                break;
+        }
+    });
+
+    console.log(`total = ${horizontal*depth}`);
 }

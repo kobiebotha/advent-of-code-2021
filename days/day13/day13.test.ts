e18db885022d369f1ef0679fc790ed9c28c21d1b
Kobie Botha <kobie@journeyapps.com>
2022-03-24 10:05:29 -0600

    day13

--- days/day13/day13.test.ts
+++ days/day13/day13.test.ts
@@ -1,8 +1,95 @@
 //FizzBuzz.test.ts
 /// <reference types="jest" />
 
-import {fizzBuzz} from "./day13";
-test("FizzBuzz test", () =>{
-    expect(fizzBuzz(2)).toBe("1 2 ");
-    expect(fizzBuzz(3)).toBe("1 2 Fizz ");
-});
+import {transform} from "./day13";
+import {reshape} from "./day13";
+
+
+let testReshapeGrid = [
+    ['.','.','.','#','#'],
+    ['#','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','#'],
+    ['#','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','.']
+]
+
+
+let x2ReshResult = [
+    ['.','.'],
+    ['#','.'],
+    ['.','.'],
+    ['.','.'],
+    ['.','.'],///
+    ['.','.'],
+    ['#','.'],
+    ['.','.'],
+    ['.','.']
+]
+let y4ReshResult = [
+    ['.','.','.','#','#'],
+    ['#','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','.']
+]
+
+let x2y4ReshResult = [
+    ['.','.'],
+    ['#','.'],
+    ['.','.'],
+    ['.','.']
+]
+
+test("reshape x = 2", () => {
+    expect(reshape(testReshapeGrid, 'x', 2)).toStrictEqual(x2ReshResult);
+})
+test("reshape y = 4", () => {
+    expect(reshape(testReshapeGrid, 'y', 4)).toStrictEqual(y4ReshResult);
+})
+
+test("reshape y = 4, x=2", () => {
+    expect(reshape(reshape(testReshapeGrid, 'y', 4), 'x', 2)).toStrictEqual(x2y4ReshResult);
+})
+
+let testGrid = 
+[
+    ['.','.','.','#','#'],
+    ['#','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','.'],
+    ['.','.','.','#','.']
+]
+let x2Result = 
+[
+    ['#','#'],
+    ['#','.'],
+    ['.','.'],
+    ['.','#'],
+    ['.','#']
+]
+
+let y2Result = 
+[
+    ['.','.','.','#','#'],
+    ['#','.','.','#','.']
+]
+
+test("fold x = 2 test", () => {
+    expect(transform(testGrid, 'x', 2)).toStrictEqual(x2Result);
+})
+
+let testGrid2 = 
+[
+    ['.','.','.','#','#'],
+    ['#','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','#','.'],
+    ['.','.','.','#','.']
+]
+
+test("fold y = 2 test", () => {
+    expect(transform(testGrid2, 'y', 2)).toStrictEqual(y2Result);
+})
--- days/day13/day13.ts
+++ days/day13/day13.ts
@@ -1,13 +1,152 @@
 const fs = require('fs')
 
+//generates an empty 2D array with m columns and n rows
+const zeros = (m: number, n: number) => [...Array(n)].map(e => Array(m).fill('.'));
+
+function loadSlide(debug: boolean): [string[][], (number | string)[][]] {
+    let results: string[] = [];
+    let grid: string[][] = [[]];
+    let instructions: (string | number)[][] = [];
+
+    let test = zeros(3, 4);
+
+
+    try {
+        let path = debug ? './inputs/day13/test' : './inputs/day13/puzzle'
+        const data = fs.readFileSync(path, 'utf8');
+
+        results = data.split('\n')
+
+        //scan once to get maxX and maxY
+        let maxX = 0, maxY = 0;
+        results.forEach(line => {
+            if (line[0] != 'f') { //coordinate pair
+                let lineX = parseInt(line.split(',')[0]);
+                let lineY = parseInt(line.split(',')[1]);
+                if (lineX > maxX) {
+                    maxX = lineX
+                };
+                if (lineY > maxY) {
+                    maxY = lineY
+                };
+            }
+        });
+
+        grid = zeros(maxX + 1, maxY + 1);
+
+        //load hashes
+        results.forEach(line => {
+            if (line[0] == 'f') { //fold
+                let insParts = line.split('=');
+                let insAxis = insParts[0][insParts[0].length - 1]; //get last char
+                let insOffset = parseInt(insParts[1]);
+                instructions.push([insAxis, insOffset]);
+            }
+            else { //load point
+                let lineX = parseInt(line.split(',')[0]);
+                let lineY = parseInt(line.split(',')[1]);
+                grid[lineY][lineX] = '#';
+            }
+        });
+
+    } catch (err) {
+        console.error(err)
+    }
+
+    return [grid, instructions];
+}
+
+function processInstructions(inputGrid: string[][], instructions: (number | string)[][]): string[][] {
+    let resultingGrid = inputGrid;
+    instructions.forEach(inst => {
+        let foldAxis = inst[0];
+        let foldOffset = inst[1];
+        //@ts-ignore
+        resultingGrid = transform(resultingGrid, foldAxis, foldOffset);
+
+        let numPoints = countPoints(resultingGrid);
+        console.log('numPoints = ', numPoints);
+    });
+    return resultingGrid;
+}
+
+function countPoints(inputGrid: string[][]) {
+    let points = 0;
+
+    for (let col = 0; col < inputGrid[0].length; col++) {
+        for (let row = 0; row < inputGrid.length; row++ ) {
+            if (inputGrid[row][col] == '#') {
+                points++;
+            }
+        }
+    }
+
+    return points;
+}
+
+export function reshape(input: string[][], fold_axis: string, fold_value: number): string[][] {
+    let result: string[][] = [];
+    let numCols = input[0].length; //x
+    let numRows = input.length; //y
+
+    if (fold_axis == 'x') { //cut columns in half
+        result = zeros(fold_value, numRows);
+    }
+    else if (fold_axis == 'y') {
+        result = zeros(numCols, fold_value);
+    }
+
+    // load values from input into result;
+    for (let col = 0; col < result[0].length; col++) {
+        for (let row = 0; row < result.length; row++) {
+            result[row][col] = input[row][col];
+        }
+    }
+
+    return result;
+}
+
 
 /**
  * 
  * operates on a grid
  * a transform is a function of the point to transform, and the axis about which to transform
  */
-function transform() {
+export function transform(input: string[][], fold_axis: string, fold_value: number): string[][] {
+    let result = input;
+    // //make sure input's row count is odd, if the fold_value is even
+    // if ((input.length % 2 == 0) || (input[0].length % 2 == 0)) {
+    //     throw new Error("dimensions should be odd");
+    // }
 
+    if (fold_axis == 'x') {
+        for (let col = fold_value+1; col < input[0].length; col++) {
+            for (let row = 0; row < input.length; row++) {
+                if (input[row][col] == '#') {
+                    let distanceFromAxis = col-fold_value;
+                    let mirrorPoint = fold_value-distanceFromAxis; 
+                    result[row][mirrorPoint] = '#'; //mirror operation
+                }
+            }
+        }
+    }
+
+    //switch y: fold bottom up
+    else if (fold_axis == 'y') {
+        for (let row = fold_value+1; row < input.length; row++) {
+            for (let col = 0; col < input[0].length; col++) {
+                if (input[row][col] == '#') {
+                    let distanceFromAxis = row-fold_value;
+                    let mirrorPoint = fold_value-distanceFromAxis; 
+                    result[mirrorPoint][col] = '#'; //mirror operation
+                }
+            }
+        }
+    }
+
+    //reshape grid
+    result = reshape(result, fold_axis, fold_value);
+    return result;
 }
 
 export function fizzBuzz(n: number): string {
@@ -27,5 +166,10 @@ export function fizzBuzz(n: number): string {
 }
 
 export function run() {
+    let data = loadSlide(false);
+
+    let results = processInstructions(data[0], data[1]);
+
     console.log('it ran')
+
 }
